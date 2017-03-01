920e5d6f6841ca2b309385e7ba94670ee40a0f06
Frocktf <scan@scandeiMac.local>
2017-03-01 17:36:13 +0800

    change file

--- Multiprocess.py
+++ Multiprocess.py
@@ -28,23 +28,54 @@
 # 	print 'Process end.'
 
 
-#use pool
-from multiprocessing import Pool
+# #use pool
+# from multiprocessing import Pool
+# import os,time,random
+
+# def long_time_task(name):
+# 	print 'Run task %s (%s)...'%(name,os.getpid())
+# 	start = time.time()
+# 	time.sleep(random.random()*3)
+# 	end = time.time()
+# 	print 'Task %s runs %0.2f seconds.' %(name,(end-start))
+
+# if __name__ == '__main__':
+# 	print 'Parent process %s.' %os.getpid()
+# 	p = Pool()
+# 	for i in range(5):
+# 		p.apply_async(long_time_task,args=(i,))
+# 	print 'Waintng for all subprocesses done...'
+# 	p.close()
+# 	p.join()
+# 	print 'All subprocesses done.'
+
+#multiprocess communicate
+
+from multiprocessing import Process,Queue
 import os,time,random
 
-def long_time_task(name):
-	print 'Run task %s (%s)...'%(name,os.getpid())
-	start = time.time()
-	time.sleep(random.random()*3)
-	end = time.time()
-	print 'Task %s runs %0.2f seconds.' %(name,(end-start))
-
+#write data
+def write(q):
+	for value in ['A','B','C']:
+		print 'Put %s to queue...'%value
+		q.put(value)
+		time.sleep(random.random())
+#read data
+def read(q):
+	while True:
+		value = q.get(True)
+		print 'Get %s from queue.'%value
 if __name__ == '__main__':
-	print 'Parent process %s.' %os.getpid()
-	p = Pool()
-	for i in range(5):
-		p.apply_async(long_time_task,args=(i,))
-	print 'Waintng for all subprocesses done...'
-	p.close()
-	p.join()
-	print 'All subprocesses done.'
+	#parent process create queue and put it to other child process.
+	q = Queue()
+	pw = Process(target=write,args=(q,))
+	pr = Process(target=read,args=(q,))
+	#start child process pw
+	pw.start()
+	#start child process pr
+	pr.start()
+	#wait for pw goto end
+	pw.join()
+	#pr process is a dead circule, it can't be wait for end, so must force to end it
+	pr.terminate()
+	
